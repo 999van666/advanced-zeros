a30b3b6718f55eec756b7a012f3a4469ce4e42b0
999van666 <paganec666.van@gmail.com>
2019-03-03 02:04:34 +0300

    feat chekk

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    let zeroCount = 0;
+      while (number) {
+        for (var base = 2; base <= 255; base++) {
+
+          for (var j = 2; j < i; j++) {
+            if (base % j == 0) continue nextPrime;
+          }
+        
+      
+          alert( base ); // простое
+        }
+        number = (number / base)+(number / base^2)+(number / base^3)+(number / base^4)+(number / base^5)+(number / base^6)+(number / 2^7)+(number / base^8) | 0;
+          zeroCount += number;
+    }
+    return zeroCount;
+  }
+  console.log(getZeroCount(8)); // 1
+  console.log(getZeroCount(12)); // 2
