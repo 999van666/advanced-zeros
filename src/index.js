fd93d5bc63ebb731827c169b2f3d3a0e83eed209
999van666 <paganec666.van@gmail.com>
2019-03-04 00:04:38 +0300

    fear git commit -m 'fear' git commit -m fear

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 module.exports = function getZerosCount(number, base) {
     let zeroCount = 0;
-   
-    for (var base = 2; base <= 255; curr++) {
-
-      for (var curr = 2; curr < i; curr++) {
-        if (i % curr == 0) continue nextPrime;
+      var result = []
       
-        num /= curr;
-        dividersNum[curr]++;
-      }
-  
-        if (dividersNum[curr] !== 0){
-           dividers.push(curr);
-      }
-curr++
-    }
+      var d = 2;
 
+      while (base > 1) {
+        if (base % d === 0) {
+          base /= d;
+          result.push(d);
+          d = 2;
+        } else {
+          d++;
+        }
+      
+      return result;
+    } 
     while (number) {
-        number = (number / dividers)+(number / dividers^2)+(number / dividers^3)+(number / dividers^4)+(number / dividers^5)+(number / dividers^6)+(number / dividers^7)+(number / dividers^8) | 0;
+        number = (number / Math.pow(d, result));
           zeroCount += number;
     }
     return zeroCount;
+  
   }
-  console.log(getZeroCount(8)); // 1
-  console.log(getZeroCount(12)); // 2
